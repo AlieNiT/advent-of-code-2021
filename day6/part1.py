ab54d03660dd97e2bbf406bcc048e8cb5b86bf28
AlieNiT <ali.blade.1380@gmail.com>
2021-12-27 03:45:08 +0330

    day6 completed.

---
+++ day6/part1.py
@@ -0,0 +1,12 @@
+fish = list(map(lambda x: int(x), input().split(",")))
+timings = [0 for _ in range(9)]
+for n in fish:
+    timings[n] += 1
+for i in range(80):
+    timings.append(timings.pop(0))
+    timings[6] += timings[-1]
+
+count = 0
+for n in timings:
+    count += n
+print(count)
